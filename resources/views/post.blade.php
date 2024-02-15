6c2de3ac6258716b8a87e325b9f0bdfef4e5cedd
LJ1319 <luka.jikia.2@btu.edu.ge>
2024-02-15 21:58:05 +0400

    feat: make a route and link to it

---
+++ resources/views/post.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+<article>
+    <h1><a href="/post">My Third Blog</a></h1>
+
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate impedit quis repellendus ullam? Accusamus
+        ad architecto cupiditate dolor labore maiores minus odit officiis quia quo, quod sed veritatis, voluptatem.
+        Adipisci amet aspernatur aut autem beatae culpa cum cumque debitis deserunt dolore doloribus eligendi est fugiat
+        illum in ipsum iusto laboriosam laborum libero minus natus necessitatibus nisi numquam odio officiis omnis quas
+        quisquam quod ratione repudiandae rerum saepe sequi tempore temporibus tenetur vitae, voluptatibus. Accusantium
+        ad adipisci debitis delectus harum minus nesciunt obcaecati totam. Alias assumenda at, consequatur dignissimos
+        doloribus enim, eos esse, libero magni quaerat quam quas quibusdam voluptates voluptatibus.
+    </p>
+
+    <a href="/">Go Back</a>
+</article>
+</body>
