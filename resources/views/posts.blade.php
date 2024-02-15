855ae622894e3a0a7e02d6150596402b5c1dde77
LJ1319 <luka.jikia.2@btu.edu.ge>
2024-02-16 02:23:12 +0400

    feat: use the filesystem class to read a directory

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,45 +4,9 @@
 <link rel="stylesheet" href="/app.css">
 
 <body>
+<?php foreach ($posts as $post) : ?>
 <article>
-    <h1><a href="/posts/my-first-post">My First Blog</a></h1>
-
-    <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate impedit quis repellendus ullam? Accusamus
-        ad architecto cupiditate dolor labore maiores minus odit officiis quia quo, quod sed veritatis, voluptatem.
-        Adipisci amet aspernatur aut autem beatae culpa cum cumque debitis deserunt dolore doloribus eligendi est fugiat
-        illum in ipsum iusto laboriosam laborum libero minus natus necessitatibus nisi numquam odio officiis omnis quas
-        quisquam quod ratione repudiandae rerum saepe sequi tempore temporibus tenetur vitae, voluptatibus. Accusantium
-        ad adipisci debitis delectus harum minus nesciunt obcaecati totam. Alias assumenda at, consequatur dignissimos
-        doloribus enim, eos esse, libero magni quaerat quam quas quibusdam voluptates voluptatibus.
-    </p>
+        <?= $post; ?>
 </article>
-
-<article>
-    <h1><a href="/posts/my-second-post">My Second Blog</a></h1>
-
-    <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate impedit quis repellendus ullam? Accusamus
-        ad architecto cupiditate dolor labore maiores minus odit officiis quia quo, quod sed veritatis, voluptatem.
-        Adipisci amet aspernatur aut autem beatae culpa cum cumque debitis deserunt dolore doloribus eligendi est fugiat
-        illum in ipsum iusto laboriosam laborum libero minus natus necessitatibus nisi numquam odio officiis omnis quas
-        quisquam quod ratione repudiandae rerum saepe sequi tempore temporibus tenetur vitae, voluptatibus. Accusantium
-        ad adipisci debitis delectus harum minus nesciunt obcaecati totam. Alias assumenda at, consequatur dignissimos
-        doloribus enim, eos esse, libero magni quaerat quam quas quibusdam voluptates voluptatibus.
-    </p>
-</article>
-
-<article>
-    <h1><a href="/posts/my-third-post">My Third Blog</a></h1>
-
-    <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate impedit quis repellendus ullam? Accusamus
-        ad architecto cupiditate dolor labore maiores minus odit officiis quia quo, quod sed veritatis, voluptatem.
-        Adipisci amet aspernatur aut autem beatae culpa cum cumque debitis deserunt dolore doloribus eligendi est fugiat
-        illum in ipsum iusto laboriosam laborum libero minus natus necessitatibus nisi numquam odio officiis omnis quas
-        quisquam quod ratione repudiandae rerum saepe sequi tempore temporibus tenetur vitae, voluptatibus. Accusantium
-        ad adipisci debitis delectus harum minus nesciunt obcaecati totam. Alias assumenda at, consequatur dignissimos
-        doloribus enim, eos esse, libero magni quaerat quam quas quibusdam voluptates voluptatibus.
-    </p>
-</article>
+<?php endforeach; ?>
 </body>
