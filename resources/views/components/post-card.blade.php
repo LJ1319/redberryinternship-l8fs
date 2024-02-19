667f68d312f718f43e580183d138275e1c84470d
LJ1319 <luka.jikia.2@btu.edu.ge>
2024-02-19 23:17:01 +0400

    refactor: extract the single blog post page into separate component

--- resources/views/components/post-card.blade.php
+++ resources/views/components/post-card.blade.php
@@ -11,9 +11,7 @@
         <div class="mt-8 flex flex-col justify-between">
             <header>
                 <div class="space-x-2">
-                    <a href="/categories/{{ $post->category->slug }}"
-                       class="px-3 py-1 border border-blue-300 rounded-full text-blue-300 text-xs uppercase font-semibold"
-                       style="font-size: 10px">{{ $post->category->name }}</a>
+                    <x-category-button :category="$post->category"/>
                 </div>
 
                 <div class="mt-4">
@@ -42,7 +40,6 @@
                         <a href="/authors/{{ $post->author->username }}">
                             <h5 class="font-bold">{{ $post->author->name }}</h5>
                         </a>
-                        <h6>Mascot at Laracasts</h6>
                     </div>
                 </div>
 
