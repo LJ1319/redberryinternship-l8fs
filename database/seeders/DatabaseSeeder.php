c808b80907ed5c059c06fadd09d329bef15302be
LJ1319 <luka.jikia.2@btu.edu.ge>
2024-02-19 16:55:33 +0400

    feat: add factories for post and category

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,52 +15,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::truncate();
-        Post::truncate();
-        Category::truncate();
-
-        $user = User::factory()->create();
-
-        $personal = Category::create([
-            'name' => 'Personal',
-            'slug' => 'personal'
+        $user = User::factory()->create([
+            'name' => 'John Doe'
         ]);
 
-        $family = Category::create([
-            'name' => 'Family',
-            'slug' => 'family'
-        ]);
-
-        $work = Category::create([
-            'name' => 'Work',
-            'slug' => 'work'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $personal->id,
-            'title' => 'My Personal Post',
-            'slug' => 'my-personal-post',
-            'excerpt' => 'Lorem ipsum dolor sit amet.',
-            'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ab animi culpa, cumque, dicta distinctio eum facere fugit ipsam labore laboriosam nesciunt nostrum perferendis, quaerat quas quisquam reprehenderit sequi vitae. Est facere iure rem sit! Ab ad amet commodi corporis debitis dolore doloremque earum esse exercitationem facilis, maxime modi necessitatibus neque non obcaecati officia pariatur reprehenderit sint vitae voluptate voluptatem voluptatibus voluptatum? Aliquid aperiam aspernatur blanditiis distinctio eaque esse, ipsum iste labore laboriosam molestias nam natus nesciunt officia officiis possimus praesentium quibusdam, quis recusandae reprehenderit voluptatibus! Dolor, enim labore molestias nobis, quia quos repudiandae rerum soluta temporibus totam unde vitae.</p>'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $family->id,
-            'title' => 'My Family Post',
-            'slug' => 'my-first-post',
-            'excerpt' => 'Lorem ipsum dolor sit amet.',
-            'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ab animi culpa, cumque, dicta distinctio eum facere fugit ipsam labore laboriosam nesciunt nostrum perferendis, quaerat quas quisquam reprehenderit sequi vitae. Est facere iure rem sit! Ab ad amet commodi corporis debitis dolore doloremque earum esse exercitationem facilis, maxime modi necessitatibus neque non obcaecati officia pariatur reprehenderit sint vitae voluptate voluptatem voluptatibus voluptatum? Aliquid aperiam aspernatur blanditiis distinctio eaque esse, ipsum iste labore laboriosam molestias nam natus nesciunt officia officiis possimus praesentium quibusdam, quis recusandae reprehenderit voluptatibus! Dolor, enim labore molestias nobis, quia quos repudiandae rerum soluta temporibus totam unde vitae.</p>'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $work->id,
-            'title' => 'My Work Post',
-            'slug' => 'my-work-post',
-            'excerpt' => 'Lorem ipsum dolor sit amet.',
-            'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ab animi culpa, cumque, dicta distinctio eum facere fugit ipsam labore laboriosam nesciunt nostrum perferendis, quaerat quas quisquam reprehenderit sequi vitae. Est facere iure rem sit! Ab ad amet commodi corporis debitis dolore doloremque earum esse exercitationem facilis, maxime modi necessitatibus neque non obcaecati officia pariatur reprehenderit sint vitae voluptate voluptatem voluptatibus voluptatum? Aliquid aperiam aspernatur blanditiis distinctio eaque esse, ipsum iste labore laboriosam molestias nam natus nesciunt officia officiis possimus praesentium quibusdam, quis recusandae reprehenderit voluptatibus! Dolor, enim labore molestias nobis, quia quos repudiandae rerum soluta temporibus totam unde vitae.</p>'
+        Post::factory(5)->create([
+            'user_id' => $user->id
         ]);
     }
 }
