038bea6ec018d0149c52af415f1aa2ab344d165f
LJ1319 <luka.jikia.2@btu.edu.ge>
2024-02-20 20:25:37 +0400

    feat: implement login functionality

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -2,9 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Validation\ValidationException;
 
 class SessionsController extends Controller
 {
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    public function store()
+    {
+        $attributes = request()->validate([
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+
+        if (!auth()->attempt($attributes)) {
+            throw ValidationException::withMessages([
+                'email' => 'Your provided credentials could not be verified.'
+            ]);
+        }
+
+        session()->regenerate();
+
+        return redirect('/')->with('success', 'Welcome Back!');
+    }
+
     public function destroy()
     {
         auth()->logout();
